e2de07092231a5e40c2d25b8daa4a9163933adf4
lukemallory <luke.mallory@mediabeacon.com>
2020-05-18 11:46:38 -0500

    java.util.Data should be a long sent over the wire
    error handling

--- src/main/java/com/google/gwtjsonrpc/server/JavaDateDeserializer.java
+++ src/main/java/com/google/gwtjsonrpc/server/JavaDateDeserializer.java
@@ -11,12 +11,23 @@ public class JavaDateDeserializer
         extends TypeAdapter<Date> {
     @Override
     public void write(JsonWriter out, Date value) throws IOException {
-        if(value != null) out.value(value.getTime());
-        else out.nullValue();
+        if(value != null) {
+            out.value(value.getTime());
+        }
+        else {
+            out.nullValue();
+        }
     }
 
     @Override
     public Date read(JsonReader in) throws IOException {
-        return new Date(in.nextLong());
+        if (in == null) {
+            return null;
+        }
+        Long aLong = in.nextLong();
+        if (aLong == null) {
+            return null;
+        }
+        return new Date(aLong);
     }
 }
